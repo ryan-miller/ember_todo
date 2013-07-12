98664ee81d12a5f84800e23680719d84a34c3286
ryan-miller <ryan.l.miller@icloud.com>
2013-07-12 14:39:33 -0400

    working app

---
+++ js/router.js
@@ -0,0 +1,40 @@
+Todos.Router.map(function () {
+  this.resource('todos', { path: '/' }, function () {
+    this.route('active')
+    this.route('completed')
+  })
+})
+
+Todos.TodosIndexRoute = Ember.Route.extend({
+  model: function () {
+    return Todos.Todo.find()
+  }
+})
+
+Todos.TodosRoute = Ember.Route.extend({
+  model: function () {
+    return Todos.Todo.find()
+  }
+})
+
+Todos.TodosCompletedRoute = Ember.Route.extend({
+  model: function () {
+    return Todos.Todo.filter(function (todo) {
+      if (todo.get('isCompleted')) { return true }
+    })
+  },
+  renderTemplate: function (controller) {
+    this.render('todos/index', {controller: controller})
+  }
+})
+
+Todos.TodosActiveRoute = Ember.Route.extend({
+  model: function () {
+    return Todos.Todo.filter(function (todo) {
+      if (!todo.get('isCompleted')) { return true }
+    })
+  },
+  renderTemplate: function(controller) {
+    this.render('todos/index', {controller: controller})
+  }
+})
